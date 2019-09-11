de8d51b8dc06993dba2b1e3b1ac81d16b6d34119
lintuming <lintuming@tuzhanai.com>
2019-09-11 09:38:45 +0800

    perf(*): eliminate the `deprecatedPrimitiveDeps()` on production mode

--- src/index.ts
+++ src/index.ts
@@ -4,38 +4,46 @@ import useShallowDeps from './useShallowDeps';
 const isPrimitive = (val: any) => val !== Object(val);
 
 const deprecatedPrimitiveDeps = (lastestDeps, hookName: string) => {
-  if (process.env.NODE_ENV !== 'production') {
-    const originName = `use${hookName.slice(10)}`;
-    if (!lastestDeps || !lastestDeps.length) {
-      return console.warn(`\`${hookName}\` should not be used with no dependencies. Use React.${originName} instead.`);
-    }
+  const originName = `use${hookName.slice(10)}`;
+  if (!lastestDeps || !lastestDeps.length) {
+    return console.warn(`\`${hookName}\` should not be used with no dependencies. Use React.${originName} instead.`);
+  }
 
-    if (lastestDeps.every(isPrimitive)) {
-      return console.warn(
-        `\`${hookName}\` should not be used with dependencies that are all primitive values. Use React.${originName} instead.`
-      );
-    }
+  if (lastestDeps.every(isPrimitive)) {
+    return console.warn(
+      `\`${hookName}\` should not be used with dependencies that are all primitive values. Use React.${originName} instead.`
+    );
   }
 };
 
 const useShallowEffect = function(cb: EffectCallback, deps: DependencyList) {
-  deprecatedPrimitiveDeps(deps, 'useShallowEffect');
+  if (process.env.NODE_ENV !== 'production') {
+    deprecatedPrimitiveDeps(deps, 'useShallowEffect');
+  }
   return React.useEffect(cb, useShallowDeps(deps));
 };
 const useShallowLayoutEffect = function(cb: EffectCallback, deps: any[]) {
-  deprecatedPrimitiveDeps(deps, 'useShallowLayoutEffect');
+  if (process.env.NODE_ENV !== 'production') {
+    deprecatedPrimitiveDeps(deps, 'useShallowLayoutEffect');
+  }
   return React.useLayoutEffect(cb, useShallowDeps(deps));
 };
 const useShallowMemo = function<T>(cb: () => T, deps: any[]) {
-  deprecatedPrimitiveDeps(deps, 'useShallowMemo');
+  if (process.env.NODE_ENV !== 'production') {
+    deprecatedPrimitiveDeps(deps, 'useShallowMemo');
+  }
   return React.useMemo<T>(cb, useShallowDeps(deps));
 };
 const useShallowCallback = function(cb: (...args: any[]) => any, deps: any[]) {
-  deprecatedPrimitiveDeps(deps, 'useShallowCallback');
+  if (process.env.NODE_ENV !== 'production') {
+    deprecatedPrimitiveDeps(deps, 'useShallowCallback');
+  }
   return React.useCallback(cb, useShallowDeps(deps));
 };
 const useShallowImperativeHandle = function<T, R extends T>(ref: Ref<T> | undefined, cb: () => R, deps: any[]) {
-  deprecatedPrimitiveDeps(deps, 'useShallowImperativeHandle');
+  if (process.env.NODE_ENV !== 'production') {
+    deprecatedPrimitiveDeps(deps, 'useShallowImperativeHandle');
+  }
   return React.useImperativeHandle(ref, cb, useShallowDeps(deps));
 };
 
